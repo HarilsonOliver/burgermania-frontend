e79f934acc50165dbbefb552af5376ceb68913aa
HPO <harilsonpires@gmail.com>
2024-11-14 08:53:45 -0300

    feat: components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PedidoComponent } from './components/pages/pedido/pedido.component';
+import { CardapioComponent } from './components/pages/cardapio/cardapio.component';
+import { CategoriaComponent } from './components/pages/categoria/categoria.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/pedido', pathMatch: 'full' },
+  { path: 'pedido', component: PedidoComponent },
+  { path: 'cardapio', component: CardapioComponent },
+  { path: 'cardapio/:categoria', component: CategoriaComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ButtonComponent } from './components/button/button.component';
+import { CardComponent } from './components/card/card.component';
+import { DescriptionComponent } from './components/description/description.component';
+import { InputComponent } from './components/input/input.component';
+import { PedidoComponent } from './components/pages/pedido/pedido.component';
+import { CardapioComponent } from './components/pages/cardapio/cardapio.component';
+import { CategoriaComponent } from './components/pages/categoria/categoria.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ButtonComponent,
+    CardComponent,
+    DescriptionComponent,
+    InputComponent,
+    PedidoComponent,
+    CardapioComponent,
+    CategoriaComponent
   ],
   imports: [
     BrowserModule,
