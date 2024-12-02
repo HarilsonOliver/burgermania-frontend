8100507cb688f54a8b9cb147dd9cd2b7a1929d01
HP <harilsonpires@gmail.com>
2024-12-02 08:25:25 -0300

    fix: implementar quantidade e preço total do produto dinamico e correção no css

--- src/app/views/pedido/pedido.component.ts
+++ src/app/views/pedido/pedido.component.ts
@@ -8,16 +8,31 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class PedidoComponent {
   produto1: string = '';
-  quantidade1: number | null = null;
+  quantidade1: number = 1;
   observacao: string = '';
+  precoProduto: number = 0; // Preço unitário do produto
+  totalPrice: number = 0; // Preço total baseado na quantidade
 
   constructor(private router: Router, private route: ActivatedRoute) {
     this.route.queryParams.subscribe((params) => {
-      console.log('Parâmetros recebidos:', params); // Adicione este log
+      console.log('Parâmetros recebidos:', params); // Log dos parâmetros
       if (params['nome']) {
-        this.produto1 = params['nome']; // Inicialize com o valor do parâmetro
+        this.produto1 = params['nome'];
+      }
+      if (params['preco']) {
+        this.precoProduto = +params['preco']; // Converte o preço para número
+        this.updateTotal(); // Atualiza o total ao inicializar
       }
     });
+  }
+
+  // Atualiza o preço total baseado na quantidade
+  updateTotal(): void {
+    if (this.quantidade1 && this.quantidade1 > 0) {
+      this.totalPrice = this.precoProduto * this.quantidade1;
+    } else {
+      this.totalPrice = 0; // Reseta o preço total se a quantidade for inválida
+    }
   }
 
   navigateTo(route: string) {
